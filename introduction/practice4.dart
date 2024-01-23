1929c1a939f8d2b9a89dc7616878c911d1d71cac
mahmud5658 <almahmuda5@gmail.com>
2024-01-23 23:50:21 +0600

    dart

---
+++ introduction/practice4.dart
@@ -0,0 +1,7 @@
+void main() {
+  dynamic name = "Abdullah Al Mahmud";
+  print(name);
+
+  name = 13;
+  print(name);
+}
---
+++ operators/practice1.dart
@@ -0,0 +1,18 @@
+void main() {
+  int a = 5;
+  int b = 4;
+  var c = a + b;
+  print("Sum of a and b is : $c");
+  var d = a - b;
+  print("The difference between a and b is :$d");
+  var e = -d;
+  print("The negation between a and b is: $e");
+  var f = a * b;
+  print("The product of a and b is:$f");
+  var g = a / b;
+  print("The division of a and b is: $g");
+  var h = b ~/ a;
+  print("The quotient of a and b is: $h");
+  var i = b % a;
+  print("The reminder of a and b is: $i");
+}

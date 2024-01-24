061ecf9f1813ed9adb60b8b81d2d363af67aaf92
mahmud5658 <almahmuda5@gmail.com>
2024-01-24 21:15:15 +0600

    dart practice

---
+++ control/practice1.dart
@@ -0,0 +1,6 @@
+void main() {
+  var num = 10;
+  for (var i=0; i <= num; i++) {
+    print(i);
+  }
+}
---
+++ data_types/practice1.dart
@@ -0,0 +1,9 @@
+void main() {
+  var number = 10;
+  print(number);
+  var result = number.isEven;
+  print(result);
+  var number2 = 10;
+  var result1 = number2.sign;
+  print(result1);
+}
